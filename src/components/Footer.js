cf3c650d3bcdf92b36e96b051d7719f5da454859
jalloh-codes <saylujalloh@gmail.com>
2022-04-11 17:03:44 -0400

    footer text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = ()=>{
     return (
         <div className="footer">
             <div className="footer-wrapper">
-                <p><span>&#169;</span> 2021</p>
+                <p><span>&#169;</span> 2022</p>
                 <p>Built with passion</p>
             </div>
         </div>
